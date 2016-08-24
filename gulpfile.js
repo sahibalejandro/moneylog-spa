d4b591c7dce730343ef8c1e0d8fe57828ea040c0
Sahib J. Leo <sahib.alejandro@gmail.com>
2016-08-23 20:19:59 -0500

    Agregado hot reload

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ var elixir = require('laravel-elixir');
 
 require('laravel-elixir-vueify');
 
+elixir.config.js.browserify.plugins.push({
+    name: 'browserify-hmr',
+    options: {},
+});
+
 elixir(function(mix) {
     mix.browserify('main.js');
 });
